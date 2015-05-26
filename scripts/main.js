3fb032d5195acddc62907eaacac7618f3e09c90e
samanderson <samuel.anderson27@yahoo.com>
2015-05-26 05:15:05 +0000

    Bower

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+require.config({
+   paths: {
+       "jquery" : "vendor/jquery/dist/jquery",
+       "backbone" : "vendor/backbone-amd/backbone",
+       "underscore" : "vendor/underscore-amd/underscore"
+   } 
+});
+    
+require(['views/app'], function(AppView){
+    
+    new AppView;
+     
+})
